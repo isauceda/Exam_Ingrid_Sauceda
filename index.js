55e611af9494538860500d874b5070dfb0379e05
isauceda <ingridsauceda.90@gmail.com>
2024-02-15 11:33:11 -0600

    examen

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+import { cliente } from './rutas/apiCliente.js'
+const port = 4000;
+
+app.get(' ', callback)
+
+app.listen(port, ()=>{
+
+    console.log(`Escuchando en el puerto ${port}`);
+});
---
+++ rutas/apiCliente.js
@@ -0,0 +1,18 @@
+import  express  from "express"
+const cliente = express();
+
+
+
+cliente.get('',(req,res)=>{
+
+res.json(cliente);
+
+})
+
+cliente.post('',(req, res)=>{
+
+    
+})
+
+export{ cliente }
+
