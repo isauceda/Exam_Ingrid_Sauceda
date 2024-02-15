bd26904a3e3548b0bfb9fec878eb7c6641d1a350
isauceda <ingridsauceda.90@gmail.com>
2024-02-15 15:44:43 -0600

    tabla cliente

--- rutas/apiCliente.js
+++ rutas/apiCliente.js
@@ -5,7 +5,7 @@ import {db} from '../Basededatos/conn.js'
 
 cliente.get('', async (req,res)=>{
 
-    const sql = `select * from tbl_cliente`
+    const sql = `select * from tbl_cliente order by id`
     const result = await db.query (sql);
 
 res.json(result);
@@ -28,5 +28,44 @@ cliente.post('', async(req,res)=>{
    res.json(result);
     
     })
+
+    cliente.put( '/:id', async(req, res)=>{
+
+        const {nombre, apellido, direccion} = req.body
+        const {id} = req.params
+
+        const params = [
+            nombre, 
+            apellido,
+            direccion,
+             id
+        ]
+
+        const sql = `update tbl_cliente
+        set
+         nombre = $1,
+         apellido = $2,
+         direccion= $3
+         where id = $4 returning *`
+
+         const result = await db.query (sql, params)
+
+        res.json(result);
+
+    })
+
+    cliente.delete( '/:id', async (req, res)=>{
+
+          const params = [
+        req.params.id];
+
+        const sql = `delete from tbl_cliente where id = $1  returning *`;
+
+         const result = await db.query (sql, params);
+
+        res.json(result);
+
+    })
+
 export{ cliente }
 
